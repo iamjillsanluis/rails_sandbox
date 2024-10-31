8857ea2728b30732257cd845e6686f2b8a04eb32
Jill San Luis <jill.devs@gmail.com>
2024-10-30 22:48:16 -0400

    Seed the database

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,17 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+require 'csv'
+require 'open-uri'
+
+url = "https://raw.githubusercontent.com/akhiltak/inspirational-quotes/refs/heads/master/Quotes.csv"
+begin
+  CSV.open(URI.open(url), headers: true, col_sep: ';').each do |row|
+    Quote.create!(content: row['QUOTE'], author: row['AUTHOR'])
+  end
+rescue OpenURI::HTTPError => e
+  puts "Failed to retrieve the file: #{e.message}"
+rescue CSV::MalformedCSVError => e
+  puts "Failed to parse the CSV: #{e.message}"
+end
